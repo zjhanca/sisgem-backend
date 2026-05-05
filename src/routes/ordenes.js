a261607242e26c599de1657598f95718cad5b468
juandi128 <juandi.11032008@gmail.com>
2026-05-05 15:10:40 -0500

    fix: ordenes route sin actualizar

--- src/routes/ordenes.js
+++ src/routes/ordenes.js
@@ -5,7 +5,6 @@ const { verificarToken, soloAdmin } = require('../middleware/auth');
 router.get('/',             verificarToken, ctrl.listar);
 router.get('/:id',          verificarToken, ctrl.detalle);
 router.post('/',            verificarToken, soloAdmin, ctrl.crear);
-router.put('/:id',          verificarToken, soloAdmin, ctrl.actualizar);
 router.patch('/:id/estado', verificarToken, soloAdmin, ctrl.cambiarEstado);
 
 module.exports = router;
